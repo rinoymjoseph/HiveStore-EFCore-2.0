98af45fcf4d05ae3129ba43782a5b58ba05a2f44
rinoy <rinoymjoseph@gmail.com>
2018-02-10 13:37:17 +0530

    added initial code

---
+++ HiveStore/HiveStore.TestConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using HiveStore.DataAccess;
+using HiveStore.DataAccess.Employee.Repository;
+using HiveStore.DataAccess.Employee.Repository.Interface;
+using HiveStore.Entity.Employee;
+using System;
+
+namespace HiveStore.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertEmployees();
+            Console.ReadKey();
+        }
+
+        private static void InsertEmployees()
+        {
+            CreateEmployee("Joy", "Smith", "Los Angeles", "USA");
+            CreateEmployee("Abram", "Ivanov", "Moscow", "Russia");
+            CreateEmployee("Aryan", "Sodhi", "Delhi", "India");
+            CreateEmployee("James", "Williams", "Sidney", "Australia");
+            CreateEmployee("Muhammed", "Emir", "Istanbul", "Turkey");
+        }
+
+        private static void CreateEmployee(string firstName, string lastName, string city, string country)
+        {
+            EmployeeEntity employee = new EmployeeEntity();
+            employee.City = city;
+            employee.Country = country;
+            employee.FirstName = firstName;
+            employee.LastName = lastName;
+            employee.CreatedBy = System.Environment.UserName;
+            employee.ModifiedBy = System.Environment.UserName;
+            employee.CreatedDate = DateTime.Now;
+            employee.ModifiedDate = DateTime.Now;
+            using (HiveDataContext hsDataContext = new HiveDataContext())
+            {
+                IEmployeeRepository empRepository = new EmployeeRepository(hsDataContext);
+                empRepository.AddEmployee(employee);
+                string message = empRepository.SaveChanges();
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # HiveStore-EFCore-2.0
+
+Prerequisites
+---------------
+1)VisualStudio 2017 Enabled with .Net Core 2.0 development
+2).Net Core 2.0 SDK
+
+
+Steps to Run the Application
+-----------------------------
+1) Open Solution in Visual Studio 2017.
+2) Open HiveDataContext.cs file on HiveStore.DataAccess file and change the 
+   DB connection string.
+3) Open Package Manage Console and select HiveStore.DataAccess as default project.
+4) Execute below command in PMC.
+   Update-Database
+5) Run HiveStore.TestConsole program.   
